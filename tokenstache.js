522beb4f27681b9df27e4a7cf51368c7e271f4f6
Cyrus Venn Casada <cvpcasada@outlook.com>
2017-12-12 17:18:20 +0800

    Improved documentation, replaced build with mircobundle

--- tokenstache.js
+++ tokenstache.js
@@ -8,17 +8,19 @@ function put(val, arr = []) {
 
 /** Tokenizes a string that contains handlebars and tries to resolve it
  *  @name tokenize
- *  @param {String} `String`		        The string we wanted to tokenize
- *  @param {Object | Function} [`view`] Optional, Resolves the handlebars in the string. Will strip the corresponding handlebars if unresolved		
- *  @returns {Array} `arr`              Results
+ *  @param {String} template - The template containing one or more {{variableNames}} every variable
+ *  @param {Object | Function} [view={}] - An object or function that will resolve the values 
+ *    for every variable names that is used in the template. If it's omitted, it'll be set to 
+ *    an empty object essentially removing all {{varName}}s in the template.	
+ *  @returns {Array} - Template where its variable names replaced with corresponding values.
  *  @example
  *    let arr = tokenize(`hello {{user}}`, {user: `jim`});  // outputs: ['hello', jim]
  */
-export function tokenize(string, view = {}) {
+export function tokenize(template, view = {}) {
   let match,
     arr = [];
     
-  while ((match = REG_EXP.exec(string)) !== null) {
+  while ((match = REG_EXP.exec(template)) !== null) {
     if (match[1]) {
       // ugggh, we want pattern matching here lol
       if (view instanceof Function) {
