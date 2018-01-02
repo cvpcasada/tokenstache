df42cb3a4a38f4474c6d1bc215ce73b4934ec512
Cyrus Venn Casada <cvpcasada@outlook.com>
2018-01-02 19:35:55 +0800

    Use String.prototype.split instead of RegExp.exec

--- tokenstache.js
+++ tokenstache.js
@@ -1,4 +1,4 @@
-const REG_EXP = /\{\{\s*(.*?)\s*\}\}|(\S+)/g;
+const REG_EXP = /({{.*?}})/g;
 
 function put(val, arr = []) {
   if (val !== undefined && val !== null) {
@@ -9,34 +9,34 @@ function put(val, arr = []) {
 /** Tokenizes a string that contains handlebars and tries to resolve it
  *  @name tokenize
  *  @param {String} template - The template containing one or more {{variableNames}} every variable
- *  @param {Object | Function} [view={}] - An object or function that will resolve the values 
- *    for every variable names that is used in the template. If it's omitted, it'll be set to 
- *    an empty object essentially removing all {{varName}}s in the template.	
+ *  @param {Object | Function} [view={}] - An object or function that will resolve the values
+ *    for every variable names that is used in the template. If it's omitted, it'll be set to
+ *    an empty object essentially removing all {{varName}}s in the template.
  *  @returns {Array} - Template where its variable names replaced with corresponding values.
  *  @example
  *    let arr = tokenize(`hello {{user}}`, {user: `jim`});  // outputs: ['hello', jim]
  */
 export function tokenize(template, view = {}) {
-  let match,
-    arr = [];
-    
-  while ((match = REG_EXP.exec(template)) !== null) {
-    if (match[1]) {
-      // ugggh, we want pattern matching here lol
+  let arr = [];
+  let tokens = template.split(REG_EXP);
+  for (let i = 0; i < tokens.length; i++) {
+    let match = tokens[i];
+
+    if (/^{/.test(match)) {
+      match = match.substring(2, match.length - 2);
       if (view instanceof Function) {
-        put(view(match[1], arr.length), arr);
+        put(view(match, arr.length), arr);
       } else if (view instanceof Object) {
-        const res = view[match[1]];
+        const res = view[match];
         if (res instanceof Function) {
-          put(res(match[1], arr.length), arr);
+          put(res(match, arr.length), arr);
         } else {
           put(res, arr);
         }
       }
-    } else if (match[2]) {
-      put(match[2], arr);
+    } else {
+      if (`` !== match) arr.push(match);
     }
   }
-
   return arr;
 }
--- tokenstache.test.js
+++ tokenstache.test.js
@@ -2,49 +2,34 @@ import { tokenize } from './tokenstache';
 
 describe(`tokenize()`, () => {
   it(`should tokenize without handlebars`, () => {
-    expect(tokenize(`hello world jimmy`)).toEqual([`hello`, `world`, `jimmy`]);
+    expect(tokenize(`hello world jimmy`)).toEqual([`hello world jimmy`]);
   });
 
   it(`should tokenize with handlebar using view object`, () => {
-    expect(tokenize(`hello world {{user}}`, { user: `jimmy` })).toEqual([
-      `hello`,
-      `world`,
-      `jimmy`
-    ]);
+    expect(tokenize(`hello world {{user}}`, { user: `jimmy` })).toEqual([`hello world `, `jimmy`]);
   });
 
   it(`should not include handlebars that doesnt resolve with view object`, () => {
-    expect(tokenize(`hello world {{notUser}}`, { user: `jimmy` })).toEqual([
-      `hello`,
-      `world`
-    ]);
+    expect(tokenize(`hello world {{notUser}}`, { user: `jimmy` })).toEqual([`hello world `]);
   });
 
   it(`can resolve view as function`, () => {
-    expect(tokenize(`hello world {{user}}`, () => `jimmy`)).toEqual([
-      `hello`,
-      `world`,
-      `jimmy`
-    ]);
+    expect(tokenize(`hello world {{user}}`, () => `jimmy`)).toEqual([`hello world `, `jimmy`]);
   });
 
   it(`can resolve view objects with functions`, () => {
-    expect(tokenize(`hello world {{user}}`, { user: () => `jimmy` })).toEqual([
-      `hello`,
-      `world`,
-      `jimmy`
-    ]);
+    expect(tokenize(`hello world {{user}}`, { user: () => `jimmy` })).toEqual([`hello world `, `jimmy`]);
   });
 
   it(`functions in view will resolve with handlebar key and id parameter`, () => {
     expect(
       tokenize(`hello world {{user}}`, { user: (key, id) => `${key} - ${id}` })
-    ).toEqual([`hello`, `world`, `user - 2`]);
+    ).toEqual([`hello world `, `user - 1`]);
   });
 
   it(`view function will also resolve with handlebar key and id parameter`, () => {
     expect(
       tokenize(`hello world {{user}}`, (key, id) => `${key} - ${id}`)
-    ).toEqual([`hello`, `world`, `user - 2`]);
+    ).toEqual([`hello world `, `user - 1`]);
   });
 });
